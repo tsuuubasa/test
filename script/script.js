f508be8d2d99bf74a7b8bfdcbd88ca09a78608cc
tsuuubasa <68677640+tsuuubasa@users.noreply.github.com>
2020-11-21 00:29:10 +0900

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -59,20 +59,5 @@ $('.nav-button').on('click',function(){
   }
 });
 
-//モーダルウィンドウの設定
-$('.js-modal-open').each(function(){
-        $(this).on('click',function(){
-           $('body').css('overflow','hidden');
-            var target = $(this).data('target');
-            var modal = document.getElementById(target);
-            $(modal).fadeIn();
-            return false;
-        });
-    });
-    $('.js-modal-close').on('click',function(){
-        $('.js-modal').fadeOut();
-        $('body').css('overflow','scroll');
-        return false;
-    });
 
 });
